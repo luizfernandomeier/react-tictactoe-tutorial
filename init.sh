f556761bfade2d62a0fbf2cf09fbaf45fdb4a90d
luizfernandomeier <lfmeier2@gmail.com>
2021-10-06 16:22:09 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,5 +10,20 @@ npm install @material-ui/core
 # install React Developer Tools Chrome extension
 
 # https://mui.com/getting-started/usage/
-# https://mui.com/customization/default-theme/
 # https://mui.com/components/css-baseline/#main-content
+
+# responsive meta tag
+# mobile first
+
+# https://mui.com/customization/theming/
+# https://mui.com/customization/default-theme/
+
+# https://mui.com/styles/api/#heading-makestyles-styles-options-hook
+
+# vh e vw -> 100 é o total
+
+# https://mui.com/customization/spacing/#custom-spacing
+
+# https://mui.com/components/app-bar/#heading-basic-app-bar
+
+npm install @material-ui/icons
