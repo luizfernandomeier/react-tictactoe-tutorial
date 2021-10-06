8aa1f7815d2e06e89ddb41eabec2f245d3e456b5
luizfernandomeier <lfmeier2@gmail.com>
2021-10-05 21:09:14 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,3 +8,6 @@ npm install @material-ui/core
 
 # install Babel JavaScript VSCode extension
 # install React Developer Tools Chrome extension
+
+# https://mui.com/getting-started/usage/
+# https://mui.com/customization/default-theme/
